edd080e5c6f42d11e84d593cef1af1956f44c58e
Djazzone <saifi.boinali@gmail.com>
2016-05-08 21:34:23 +0200

    Ajout de Lorem Lorem

--- demo.php
+++ demo.php
@@ -15,4 +15,5 @@ hello('Gideon');
 
 <p>
     Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid blanditiis consectetur, dolores, earum harum id iste labore magni minus mollitia nulla officia quae quaerat quas quidem sit vel voluptas? Non.
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus accusantium ad aliquid at, beatae cum dolorem dolorum ducimus fugit harum magnam mollitia nobis optio pariatur possimus quasi qui rem reprehenderit.
 </p>
